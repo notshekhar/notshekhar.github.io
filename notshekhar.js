5cadefeebec4f18cd44e4625c47e4cf47f247d7b
notshekhar <notshekhar@gmail.com>
2019-07-06 11:10:07 +0530

    new

--- notshekhar.js
+++ notshekhar.js
@@ -10,6 +10,11 @@ let toptabs = document.querySelectorAll('.toptab')
 let about = document.querySelector('.about')
 let talks = document.querySelector('.talks')
 let talkbody = document.querySelector('.talkbody')
+let root  = document.documentElement
+let nightMode = false
+if(localStorage.getItem('nightMode')){
+	nightMode = JSON.parse(localStorage.getItem('nightMode'))
+}
 let data
 let talkdata
 let highlight = 0
@@ -96,44 +101,19 @@ window.onscroll = () => {
 	let y = window.scrollY
 	if(window.innerWidth<700){
 		if(y>=15+window.innerHeight+tabHolder.offsetHeight){
-			tabHolder.style.position = 'fixed'
-			tabHolder.style.background = 'white'
-			tabHolder.style.top = '0px'
-			tabHolder.style.left = '0px'
-			tabHolder.style.marginTop = '8px'
-			tabHolder.style.boxShadow = 'rgba(60, 64, 67, 0.15) 5px 0px 3px 1px, rgba(60, 64, 67, 0.3) 4px -1px 2px 0px'
-			spacer.style.height = `${38+tabHolder.getBoundingClientRect().height}px`
+			tabHolder.classList.add('tab_fixed_for_mobile')
 		}else{
-			tabHolder.style.position = 'relative'
-			tabHolder.style.background = 'rgba(0,0,0,0)'
-			tabHolder.style.top = 'auto'
-			tabHolder.style.left = 'auto'
-			tabHolder.style.marginTop = '30px'
-			tabHolder.style.boxShadow = 'none'
-			spacer.style.height = '0px'
+			tabHolder.classList.remove('tab_fixed_for_mobile')
+			
 		}
 	}
 }
 code.onscroll = () => {
 	let y = code.scrollTop
 	if(y>=tabHolder.offsetHeight){
-		tabHolder.style.position = 'fixed'
-		tabHolder.style.background = 'white'
-		tabHolder.style.top = '0px'
-		tabHolder.style.left = `${profile.getBoundingClientRect().width}px`
-		tabHolder.style.marginTop = '8px'
-		tabHolder.style.boxShadow = 'rgba(60, 64, 67, 0.15) 5px 0px 3px 1px, rgba(60, 64, 67, 0.3) 4px -1px 2px 0px'
-		tabHolder.style.width = `${code.getBoundingClientRect().width}px`
-		spacer.style.height = `${20+tabHolder.getBoundingClientRect().height}px`
+		tabHolder.classList.add('tab_fixed')
 	}else{
-		tabHolder.style.position = 'relative'
-		tabHolder.style.background = 'rgba(0,0,0,0)'
-		tabHolder.style.top = 'auto'
-		tabHolder.style.left = 'auto'
-		tabHolder.style.marginTop = '30px'
-		tabHolder.style.boxShadow = 'none'
-		tabHolder.style.width = '100%'
-		spacer.style.height = '0px'
+		tabHolder.classList.remove('tab_fixed')
 	}
 }
 
@@ -673,6 +653,30 @@ if(window.name == 'projects'){
 		talkdata = data 
 		talkPosts()
 	})
+}
+function changeMode(){
+	if (nightMode) {
+		root.style.setProperty('--bg-color', '#1e1e1e')
+		root.style.setProperty('--bg1-color', '#252526')
+		root.style.setProperty('--color', '#adadad')
+		root.style.setProperty('--highlight-color', '#37373d')
+		root.style.setProperty('--border', 'none')
+		document.querySelector("#nightmode").innerHTML = `<svg viewBox="0 0 26 26"><g><path d="M13 20.673c-.414 0-.75-.336-.75-.75V14.46c0-.414.336-.75.75-.75s.75.336.75.75v5.463c0 .414-.336.75-.75.75z"></path><path d="M13 4.5c-4.687 0-8.5 3.813-8.5 8.5 0 3.424 2.037 6.494 5.19 7.83.094.04.196.067.306.067.414 0 .75-.336.75-.75 0-.31-.188-.575-.456-.69C7.685 18.36 6 15.826 6 13c0-3.86 3.14-7 7-7s7 3.14 7 7c0 2.825-1.683 5.358-4.288 6.454-.273.112-.467.38-.467.693 0 .414.336.75.75.75.105 0 .204-.022.294-.06C19.455 19.51 21.5 16.433 21.5 13c0-4.687-3.813-8.5-8.5-8.5zm4 18.25c0 .414-.34.75-.757.75H9.757c-.418 0-.757-.336-.757-.75s.34-.75.757-.75h6.486c.418 0 .757.336.757.75zm-1 2.5c0 .414-.34.75-.757.75h-4.486c-.418 0-.757-.336-.757-.75s.34-.75.757-.75h4.486c.418 0 .757.336.757.75z"></path></g></svg>`
+	} else {
+		root.style.setProperty('--bg-color', 'white')
+		root.style.setProperty('--bg1-color', 'white')
+		root.style.setProperty('--color', 'rgb(20, 20, 20)')
+		root.style.setProperty('--highlight-color', '#f2f2f2')
+		root.style.setProperty('--border', 'solid')
+		document.querySelector("#nightmode").innerHTML = `<svg viewBox="0 0 26 26"><g><path d="M13 20.673c-.414 0-.75-.336-.75-.75V14.46c0-.414.336-.75.75-.75s.75.336.75.75v5.463c0 .414-.336.75-.75.75z"></path><path d="M13 4.5c-4.687 0-8.5 3.813-8.5 8.5 0 3.424 2.037 6.494 5.19 7.83.094.04.196.067.306.067.414 0 .75-.336.75-.75 0-.31-.188-.575-.456-.69C7.685 18.36 6 15.826 6 13c0-3.86 3.14-7 7-7s7 3.14 7 7c0 2.825-1.683 5.358-4.288 6.454-.273.112-.467.38-.467.693 0 .414.336.75.75.75.105 0 .204-.022.294-.06C19.455 19.51 21.5 16.433 21.5 13c0-4.687-3.813-8.5-8.5-8.5zM13 3c-.414 0-.75-.34-.75-.757V.757C12.25.34 12.586 0 13 0s.75.34.75.757v1.486c0 .418-.336.757-.75.757zM3 13c0 .414-.34.75-.757.75H.757C.34 13.75 0 13.414 0 13s.34-.75.757-.75h1.486c.418 0 .757.336.757.75zm23 0c0 .414-.34.75-.757.75h-1.486c-.418 0-.757-.336-.757-.75s.34-.75.757-.75h1.486c.418 0 .757.336.757.75zM5.93 5.93c-.294.292-.77.29-1.067-.006l-1.05-1.05c-.296-.296-.298-.774-.005-1.066s.77-.29 1.066.005l1.05 1.05c.295.296.298.773.005 1.066zm16.262-2.122c.293.293.29.77-.005 1.066l-1.05 1.05c-.296.296-.774.3-1.067.006s-.29-.77.005-1.066l1.05-1.05c.297-.297.775-.3 1.067-.006zM17 22.75c0 .414-.34.75-.757.75H9.757c-.418 0-.757-.336-.757-.75s.34-.75.757-.75h6.486c.418 0 .757.336.757.75zm-1 2.5c0 .414-.34.75-.757.75h-4.486c-.418 0-.757-.336-.757-.75s.34-.75.757-.75h4.486c.418 0 .757.336.757.75z"></path></g></svg>`
+	}
+}
+changeMode()
+
+document.querySelector("#nightmode").onclick = () => {
+	nightMode = !nightMode
+	localStorage.setItem("nightMode", nightMode)
+	changeMode()
 }
 //for donation 
 // let donate = document.querySelector("#donate")
