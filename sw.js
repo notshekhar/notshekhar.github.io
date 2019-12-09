cabb54799f8a08e07c7059596d5eb18ef4348df4
notshekhar <notshekhar@gmail.com>
2019-12-09 23:40:33 +0530

    new

--- sw.js
+++ sw.js
@@ -1,14 +1,13 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
-if(workbox){
-  console.log('workbox')
-}
+workbox.precaching.precacheAndRoute(['./index.html'])
 workbox.routing.registerRoute(
   /\.js$/,
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'js-cache'
   })
 )
+
 workbox.routing.registerRoute(
   /\.json$/,
   new workbox.strategies.StaleWhileRevalidate({
@@ -16,11 +15,8 @@ workbox.routing.registerRoute(
   })
 )
 workbox.routing.registerRoute(
-  // Cache image files.
   /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
-  // Use the cache if it's available.
   new workbox.strategies.CacheFirst({
-    // Use a custom cache name.
     cacheName: 'image-cache',
     plugins: [
       new workbox.expiration.Plugin({
@@ -31,19 +27,21 @@ workbox.routing.registerRoute(
   })
 )
 workbox.routing.registerRoute(
-  // Cache image files.
   /\.(?:ttf|woff)$/,
-  // Use the cache if it's available.
   new workbox.strategies.CacheFirst({
-    // Use a custom cache name.
     cacheName: 'font-cache',
     plugins: [
       new workbox.expiration.Plugin({
-        // Cache only 20 images.
         maxEntries: 20,
         // Cache for a maximum of a week.
         maxAgeSeconds: 7 * 24 * 60 * 60,
       })
     ],
+  })
+)
+workbox.routing.registerRoute(
+  /\.css$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'css-cache'
   })
 )
