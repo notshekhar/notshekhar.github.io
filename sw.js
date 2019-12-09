edf5f7fdc91014674f36a4a1e768130a3514103d
notshekhar <notshekhar@gmail.com>
2019-12-09 23:02:52 +0530

    new

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
 
-
-
-
+if(workbox){
+  console.log('workbox')
+}
 workbox.routing.registerRoute(
   /\.js$/,
   new workbox.strategies.StaleWhileRevalidate()
