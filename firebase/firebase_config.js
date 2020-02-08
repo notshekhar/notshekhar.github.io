8c86b06f92af251eeeb9e09b4330cc9820af482b
Shekhar Tyagi <notshekhar@gmail.com>
2020-02-08 12:04:13 +0530

    new

---
+++ firebase/firebase_config.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyDP6i7eqeCR7hEMH7WWi1T7UaWmOKy9xtc",
+    authDomain: "notshekhar-306b4.firebaseapp.com",
+    databaseURL: "https://notshekhar-306b4.firebaseio.com",
+    projectId: "notshekhar-306b4",
+    storageBucket: "notshekhar-306b4.appspot.com",
+    messagingSenderId: "200187477779",
+    appId: "1:200187477779:web:a1074674d391a25c2824e6",
+    measurementId: "G-YE36QV9XSZ"
+}
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
