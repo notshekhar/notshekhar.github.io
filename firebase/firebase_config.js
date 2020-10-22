7080fe8736f37cc65582b8008f16ff96ec79be06
Shekhar Tyagi <notshekhar@gmail.com>
2020-10-22 10:04:57 +0530

    moved secret keys to .env file

--- firebase/firebase_config.js
+++ firebase/firebase_config.js
@@ -1,12 +1,12 @@
 let firebaseConfig = {
-    apiKey: "AIzaSyDP6i7eqeCR7hEMH7WWi1T7UaWmOKy9xtc",
-    authDomain: "notshekhar-306b4.firebaseapp.com",
-    databaseURL: "https://notshekhar-306b4.firebaseio.com",
-    projectId: "notshekhar-306b4",
-    storageBucket: "notshekhar-306b4.appspot.com",
-    messagingSenderId: "200187477779",
-    appId: "1:200187477779:web:a1074674d391a25c2824e6",
-    measurementId: "G-YE36QV9XSZ"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
 }
 firebase.initializeApp(firebaseConfig)
 firebase.analytics()
