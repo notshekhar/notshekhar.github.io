680d79c8318b32d83db045d4f4b0f65a43b12790
Shekhar Tyagi <notshekahr@gmail.com>
2021-10-23 18:19:45 +0530

    new

--- js/theme.js
+++ js/theme.js
@@ -7,7 +7,7 @@ if (localStorage.getItem("nightMode")) {
 // console.log(nightMode)
 
 function setVariable(root, prop, val) {
-    root.style.setProperty(prop, val)
+    root.style.setProperty(`--${prop}`, val)
 }
 function setVariables(props) {
     for (let prop in props) {
@@ -16,25 +16,25 @@ function setVariables(props) {
 }
 
 const light = {
-    "--bg-color": "#f2f2f2",
-    "--bg1-color": "white",
-    "--color": "rgb(80, 80, 80)",
-    "--highlight-color": "#f2f2f2",
-    "--border-color": "white",
-    "--header-color": "#7BE4D5",
-    "--link-color": "#00ccc0",
-    "--shadow":
+    "bg-color": "#f2f2f2",
+    "bg1-color": "white",
+    "color": "rgb(80, 80, 80)",
+    "highlight-color": "#f2f2f2",
+    "border-color": "white",
+    "header-color": "#7BE4D5",
+    "link-color": "#00ccc0",
+    "shadow":
         "0 1px 3px 1px rgba(60, 64, 67, 0.15), 0 1px 2px 0 rgba(60, 64, 67, 0.3)",
 }
 const dark = {
-    "--bg-color": "#1e1e1e",
-    "--bg1-color": "#252526",
-    "--color": "#adadad",
-    "--highlight-color": "#37373d",
-    "--border-color": "#1e1e1e",
-    "--header-color": "#1e1e1e",
-    "--link-color": "white",
-    "--shadow":
+    "bg-color": "#1e1e1e",
+    "bg1-color": "#252526",
+    "color": "#adadad",
+    "highlight-color": "#37373d",
+    "border-color": "#1e1e1e",
+    "header-color": "#1e1e1e",
+    "link-color": "white",
+    "shadow":
         "0 1px 3px 1px rgba(0, 0, 0, 0.51),    0 1px 2px 0 rgba(60, 64, 67, 0.3)",
 }
 function darkMode() {
